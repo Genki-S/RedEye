6653397dfa94865a15e37033488b177ad8e47094
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2014-08-30 13:04:17 +0900

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+my_uid = '681364011953743'
+me = User.create(name: 'Genki Sugimoto', provider: 'facebook', uid: my_uid)
+
+now = Time.parse('08:00')
+
+(1..10).each do |i|
+  start_time = now + i.hour
+  finish_time = start_time + 25.minutes
+  p = Pomodoro.create(title: "Task #{i}", started_at: start_time, finished_at: finish_time)
+
+  evaluation = [-1, 0, 1].shuffle.first
+  r = Reflection.create(evaluation: evaluation)
+
+  p.user = me
+  p.reflection = r
+  p.save
+end
